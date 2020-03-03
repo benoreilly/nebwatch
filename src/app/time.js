57b82c3d1b8a472e4844640c115f2bad72164a6f
Ben OReillygit config --global user.email Ben <neb.oreilly@gmail.com>
2020-03-02 22:59:57 -0500

    date and time updates

--- src/app/time.js
+++ src/app/time.js
@@ -26,3 +26,23 @@ toggleBtn.addEventListener('click', function() {
 resetBtn.addEventListener('click', function() {
     watch.reset();
 });
+
+
+
+// Get date
+
+let d = new Date();
+let y = d.toLocaleDateString('en-US');
+var today = document.getElementById('tDate').innerText = y;
+
+
+//Get time
+myTimer();
+var timeVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+let d = new Date();
+//let t = d.toLocaleTimeString();
+let t = d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+var tTime = document.getElementById('tTime').innerText = t;
+}
